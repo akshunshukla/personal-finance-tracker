7328017595a6b303f993e6b1ae25cddfca2609cb
Akshun SHukla <akshunshukla123@gmail.com>
2025-12-30 02:07:40 +0530

    implement login api

--- backend/src/modules/auth/auth.controller.js
+++ backend/src/modules/auth/auth.controller.js
@@ -1,10 +1,16 @@
 import asyncHandler from "../../utils/asyncHandler.js";
 import ApiResponse from "../../utils/ApiResponse.js";
 import { registerUser } from "./auth.service.js";
+import { loginUser } from "./auth.service.js";
 
 export const register = asyncHandler(async (req, res) => {
   const user = await registerUser(req.body);
   res
     .status(201)
     .json(new ApiResponse(201, user, "User registered successfully"));
+});
+
+export const login = asyncHandler(async (req, res) => {
+  const data = await loginUser(req.body);
+  res.status(200).json(new ApiResponse(200, data, "Login successful"));
 });
--- backend/src/modules/auth/auth.service.js
+++ backend/src/modules/auth/auth.service.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import pool from "../../config/db.js";
 import ApiError from "../../utils/ApiError.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async ({ name, email, password }) => {
   const existingUser = await pool.query(
@@ -22,4 +23,38 @@ export const registerUser = async ({ name, email, password }) => {
   );
 
   return result.rows[0];
+};
+
+export const loginUser = async ({ email, password }) => {
+  const result = await pool.query(
+    "SELECT id, name, email, password_hash, role FROM users WHERE email = $1",
+    [email]
+  );
+
+  if (result.rows.length === 0) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+
+  const user = result.rows[0];
+
+  const isPasswordValid = await bcrypt.compare(password, user.password_hash);
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid credentials");
+  }
+
+  const token = jwt.sign(
+    { userId: user.id, role: user.role },
+    process.env.JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  return {
+    token,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    },
+  };
 };
